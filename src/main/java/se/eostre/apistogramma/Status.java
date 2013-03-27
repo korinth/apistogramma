1a5893b6a39604367daa9e369341a0ecc7f15915
Martin Korinth <martin@korinth.se>
2013-03-27 20:38:15 +0100

    Added status exception

---
+++ src/main/java/se/eostre/apistogramma/Status.java
@@ -0,0 +1,23 @@
+package se.eostre.apistogramma;
+
+public class Status extends Exception {
+
+	private static final long serialVersionUID = 5114127077449598769L;
+	
+	int code = 200;
+	
+	public Status(String message) {
+		super(message);
+	}
+	
+	public Status(String message, int code) {
+		super(message);
+		this.code = code;
+	}
+	
+	public Status(String message, int code, Throwable cause) {
+		super(message, cause);
+		this.code = code;
+	}
+
+}
